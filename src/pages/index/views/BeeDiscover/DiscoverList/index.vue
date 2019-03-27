558f6dd3503fbab9b3946a4f43e701b589867c72
巧克力冰激凌 <121017405@qq.com>
2019-03-27 16:53:12 +0800

    路由设置

---
+++ src/pages/index/views/BeeDiscover/DiscoverList/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    发现公益项目/集市
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+</style>
--- src/route/router.js
+++ src/route/router.js
@@ -1,23 +1,75 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/index/views/Home.vue'
+import Home from '@/pages/index/views/BeeHome'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
-    name: 'home',
+    name: 'BeeHome',
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-        import(/* webpackChunkName: "about" */ '@/pages/index/views/About.vue')
+    path: '/category',
+    name: 'BeeCategory',
+    component: () => import('@/pages/index/views/BeeCategory'),
+    children: [{
+      path: '/',
+      name: 'CategoryList',
+      component: () => import('@/pages/index/views/BeeCategory/CategoryList')
+    }, {
+      path: 'details',
+      name: 'CommodityDetails',
+      component: () => import('@/pages/index/views/BeeCategory/CommodityDetails')
+    }]
+  },
+  {
+    path: '/discover',
+    name: 'BeeDiscover',
+    component: () => import('@/pages/index/views/BeeDiscover'),
+    children: [{
+      path: '/',
+      name: 'DiscoverList',
+      component: () => import('@/pages/index/views/BeeDiscover/DiscoverList')
+    }]
+  },
+  {
+    path: '/cart',
+    name: 'BeeCart',
+    component: () => import('@/pages/index/views/BeeCart'),
+    children: [{
+      path: '/',
+      name: 'CartList',
+      component: () => import('@/pages/index/views/BeeCart/CartList')
+    },
+    {
+      path: '/share',
+      name: 'CartShare',
+      component: () => import('@/pages/index/views/BeeCart/CartShare')
+    }
+    ]
+  },
+  {
+    path: '/persion',
+    name: 'BeePersion',
+    component: () => import('@/pages/index/views/BeePersion'),
+    children: [{
+      path: '/',
+      name: 'PersionalCenter',
+      component: () => import('@/pages/index/views/BeePersion/PersionalCenter')
+    },
+    {
+      path: '/order',
+      name: 'MyOrder',
+      component: () => import('@/pages/index/views/BeePersion/MyOrder')
+    }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'BeeLogin',
+    component: () => import('@/pages/index/views/BeeLogin')
   }
   ]
 })
