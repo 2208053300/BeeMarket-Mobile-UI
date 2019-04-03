dddd16bd2f476b884038410a8b371c40899b52a9
巧克力冰激凌 <121017405@qq.com>
2019-04-03 17:42:20 +0800

    移动端点击延迟插件

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -10,6 +10,9 @@ import '@/utils/flexiable'
 // NOTE 全局引入vant所有组件
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// NOTE 移动端点击延迟
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
 Vue.use(Vant)
 // NOTE 修改页面meta数据
 Vue.use(Meta)
