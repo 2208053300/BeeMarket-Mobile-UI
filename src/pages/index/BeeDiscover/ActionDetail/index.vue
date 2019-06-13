590e761afcd9bc2ae09733e1af234c262c9534f2
yangchao930915 <2208053300@qq.com>
2019-06-13 15:37:24 +0800

    行动

--- src/pages/index/BeeDiscover/ActionDetail/index.vue
+++ src/pages/index/BeeDiscover/ActionDetail/index.vue
@@ -72,7 +72,10 @@
 <script>
 import { BeeDefault } from '@/styles/index/variables.less'
 // 行动详情，参与助力，发起助力
-import { getActionDetail, joinAction } from '@/api/BeeApi/action'
+import {
+  getActionDetail
+  // joinAction
+} from '@/api/BeeApi/action'
 import detailCard0 from './components/detailCard0'
 import detailCard1 from './components/detailCard1'
 import detailCard2 from './components/detailCard2'
