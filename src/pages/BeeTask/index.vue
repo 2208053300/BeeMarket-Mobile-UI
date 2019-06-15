89d0f3edb121755dca798e6b9c76936524a31b92
巧克力冰激凌 <121017405@qq.com>
2019-06-15 14:22:56 +0800

    蜂任务详情头错误

--- src/pages/BeeTask/index.vue
+++ src/pages/BeeTask/index.vue
@@ -1,28 +1,5 @@
 <template>
   <div>
-    <bee-header v-show="$store.state.app.beeHeader" />
-    <router-view
-      style="height:100%"
-      :class="{ showHeader: $store.state.app.beeHeader }"
-    />
+    <router-view />
   </div>
 </template>
-
-<script>
-import BeeHeader from '@/components/index/BeeHeader'
-export default {
-  metaInfo: {
-    title: '蜂任务'
-  },
-  components: {
-    BeeHeader
-  }
-}
-</script>
-
-<style lang="less">
-.showHeader {
-  padding-top: 23px;
-  box-sizing: border-box;
-}
-</style>
