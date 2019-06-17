68ad5d9ae73083a4a438c41c19007fe43163d172
Jethro <jethrocup@gmail.com>
2019-06-17 16:54:02 +0800

    修改大小写错误

--- src/route/index.js
+++ src/route/index.js
@@ -370,7 +370,7 @@ const router = new Router({
           children: [
             {
               path: '',
-              component: () => import('@/pages/index/BeePersion/Profile/ProfileIndex')
+              component: () => import('@/pages/index/BeePersion/Profile/profileIndex')
             },
             {
               path: 'baseInfo',
