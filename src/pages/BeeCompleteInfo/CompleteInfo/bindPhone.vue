75f9387f1327abeb4c41383abd7871d5c3be53e7
yangchao930915 <2208053300@qq.com>
2019-06-21 19:45:05 +0800

    微信用户新手专享绑定手机号

--- src/pages/BeeCompleteInfo/CompleteInfo/bindPhone.vue
+++ src/pages/BeeCompleteInfo/CompleteInfo/bindPhone.vue
@@ -61,7 +61,7 @@ export default {
   },
   methods: {
     bindPhone() {
-      window.location.href = '/#/login'
+      window.location.href = '/#/persion/profile/accountBind/bindPhone'
     }
   }
 }
