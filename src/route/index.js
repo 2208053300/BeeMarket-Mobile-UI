8a23ce3ebfc46a35b1333e55bb551669622eef2e
yangchao930915 <2208053300@qq.com>
2019-06-22 17:56:39 +0800

    防水墙测试

--- src/route/index.js
+++ src/route/index.js
@@ -129,7 +129,7 @@ const router = new Router({
             },
             {
               path: 'pay',
-              name: 'FriendPay',
+              name: 'friendPay',
               component: () => import('@/pages/BeeFriends/Pay')
             },
             {
