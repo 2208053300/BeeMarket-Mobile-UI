3e7d913dad42be5f3e82eaecb421a9dba43a1f21
巧克力冰激凌 <121017405@qq.com>
2019-06-22 14:21:54 +0800

    修复蜂友圈跳转加载顺序

--- src/pages/BeeFriends/MyBeeFriends/index.vue
+++ src/pages/BeeFriends/MyBeeFriends/index.vue
@@ -160,18 +160,18 @@ export default {
   },
   computed: {},
   watch: {},
-  async beforeCreate() {
-    // 初始化实例之前判断该用户蜂友圈状态跳转页面
-    await this.$store.dispatch('GerUserStatus')
-    // 0 非合伙人 1 合伙人 2 冻结
-    if (this.$store.state.user.userStatus === 0) {
-      this.$router.replace({ name: 'introduction' })
-    } else if (this.$store.state.user.userStatus === 1) {
-      this.$router.replace({ name: 'beeFriends' })
-    } else if (this.$store.state.user.userStatus === 2) {
-      this.$router.replace({ name: 'freeze' })
-    }
-  },
+  // async beforeCreate() {
+  //   // 初始化实例之前判断该用户蜂友圈状态跳转页面
+  //   await this.$store.dispatch('GerUserStatus')
+  //   // 0 非合伙人 1 合伙人 2 冻结
+  //   if (this.$store.state.user.userStatus === 0) {
+  //     this.$router.replace({ name: 'introduction' })
+  //   } else if (this.$store.state.user.userStatus === 1) {
+  //     this.$router.replace({ name: 'beeFriends' })
+  //   } else if (this.$store.state.user.userStatus === 2) {
+  //     this.$router.replace({ name: 'freeze' })
+  //   }
+  // },
   created() {},
   mounted() {
     this.$store.state.app.beeHeader = true
