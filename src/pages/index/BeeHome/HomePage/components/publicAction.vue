4647590e4a1d193242300a0dbf9df47793313a8a
巧克力冰激凌 <121017405@qq.com>
2019-06-22 15:05:51 +0800

    首页行动蒙版

--- src/pages/index/BeeHome/HomePage/components/publicAction.vue
+++ src/pages/index/BeeHome/HomePage/components/publicAction.vue
@@ -16,6 +16,7 @@
       <div class="action-subhead">
         {{ homeData.charity_action.action.subtitle }}
       </div>
+      <div class="body-overlay" />
     </div>
   </div>
 </template>
@@ -61,14 +62,28 @@ export default {
     height: 1.76rem;
     padding: 0.4rem 0.3rem 0.32rem;
     box-sizing: border-box;
+    position: relative;
     .action-title {
+      position: relative;
       font-size: 0.3rem;
       color: #ffffff;
+      z-index: 1;
     }
     .action-subhead {
+      position: relative;
       font-size: 0.24rem;
       color: #ffffff;
       margin-top: 0.2rem;
+      z-index: 1;
+    }
+    .body-overlay {
+      height: 1.76rem;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.4);
+      z-index: 0;
     }
   }
 }
