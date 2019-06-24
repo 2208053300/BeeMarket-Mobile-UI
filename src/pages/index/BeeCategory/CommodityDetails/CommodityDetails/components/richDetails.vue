39beb8f78a9ac9234bc9f89dc9d8c348962e26fd
Jethro <jethrocup@gmail.com>
2019-06-24 16:45:48 +0800

    修正商品详情描述

--- src/pages/index/BeeCategory/CommodityDetails/CommodityDetails/components/richDetails.vue
+++ src/pages/index/BeeCategory/CommodityDetails/CommodityDetails/components/richDetails.vue
@@ -3,16 +3,8 @@
   <div class="rich-details">
     <div class="product-desc">
       <!-- eslint-disable-next-line -->
-      <iframe
-        id="productIframe"
-        ref="productIframe"
-        :srcdoc="descHtml"
-        frameborder="0"
-        class="desc-iframe"
-        scrolling="no"
-      />
+      <div ref="productDesc" />
     </div>
-    <!-- TODO 没有商品详情富文本时显示 -->
     <div
       v-if="!commodityData.desc_url"
       class="no-details"
@@ -27,7 +19,8 @@
 </template>
 
 <script>
-import axios from 'axios'
+// 导入获取商品描述接口
+import { getProductDesc } from '@/api/BeeApi/product'
 export default {
   components: {},
   props: {
@@ -55,51 +48,112 @@ export default {
     }
   },
   created() {},
-  mounted() {
-    // console.log(this.$refs.productIframe.contentDocument.querySelectAll('img'))
-  },
-  beforeDestroy() {
-    clearInterval(this.interval)
-  },
+  mounted() {},
+  beforeDestroy() {},
   methods: {
-    // 设置详情高度
-    setHeight() {
-      if (this.interval) {
-        return
-      }
-      this.interval = setInterval(() => {
-        const iframe = document.querySelector('#productIframe')
-        if (iframe && this.descHtml) {
-          const iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow
-          if (iframeWin.document.body) {
-            const height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight
-            iframe.style.height = `${height}px`
-            console.log('测量高度', iframe.style.height)
-          }
-        }
-      }, 1000)
-    },
     // 加载描述html
     async loadDesc() {
-      const service = axios.create({
-        baseURL: process.env.BASE_API, // api 的 base_url
-        timeout: 50000 // 请求超时时间(现在是50秒)
-      })
-      const res = await service({
-        url: '/product/desc',
-        method: 'get',
-        params: {
-          pid: this.commodityData.pid
+      try {
+        const res = await getProductDesc({
+          pid: this.commodityData.pid,
+          source: 'H5'
+        })
+        // 将服务器返回的html转换为虚拟dom
+        const html = res.data.desc
+        const dom = document.createElement('div')
+        dom.innerHTML = html
+        // 处理img标签，用于图片懒加载
+        const imgs = dom.querySelectorAll('img')
+        imgs.forEach(img => {
+          img.setAttribute('alt', `loading`)
+          img.setAttribute('data-src', img.src)
+          img.setAttribute('src', 'https://api2.fengjishi.com.cn/assets/images/loading.gif')
+        })
+        // 将处理好的html设置到div
+        this.$refs['productDesc'].innerHTML = dom.innerHTML
+        // 将新的img标签初始化懒加载
+        this.lazy()
+      } catch (e) {
+        this.descHtml = `<p>${e}</p>`
+      }
+    },
+    // 设置商品详情描述的图片懒加载
+    lazy() {
+      const Echo = (function(window, document) {
+        'use strict'
+        const store = []
+        let offset // 加载距离
+        let throttle // 加载图片时间
+        let firstnum // 首页显示图片数量
+        let poll
+        const _inView = function(el, num) {
+          let inView
+          if (num <= firstnum) {
+            inView = true
+          } else {
+            const coords = el.getBoundingClientRect()
+            inView = (coords.top >= 0 && coords.left >= 0 && coords.top) <=
+              (window.innerHeight || document.documentElement.clientHeight) + parseInt(offset)
+          }
+          return inView
         }
+
+        const _pollImages = function() {
+          let num = 0
+          for (let i = 0; i < store.length; i++) {
+            const self = store[i]
+            num++
+            if (_inView(self, num)) {
+              self.src = self.getAttribute('data-src')
+            }
+          }
+          const nodes = document.querySelectorAll('.wscnph')
+          if (nodes && nodes.length < 3) {
+            for (let j = 0; j < nodes.length; j++) {
+              nodes[j].style.height = 'auto'
+            }
+          } else {
+            for (let k = 0; k < 3; k++) {
+              nodes[k].style.height = 'auto'
+            }
+          }
+        }
+        const _throttle = function() {
+          clearTimeout(poll)
+          poll = setTimeout(_pollImages, throttle)
+        }
+        const init = function(obj) {
+          const nodes = document.querySelectorAll('.wscnph')
+          const opts = obj || {}
+          offset = opts.offset || 0
+          throttle = opts.throttle || 250
+          firstnum = opts.firstnum || 0
+          for (let i = 0; i < nodes.length; i++) {
+            store.push(nodes[i])
+          }
+          _throttle()
+          if (document.addEventListener) {
+            window.addEventListener('scroll', _throttle, false)
+          } else {
+            window.attachEvent('onscroll', _throttle)
+          }
+        }
+        return {
+          init: init,
+          render: _throttle
+        }
+      })(window, document)
+      Echo.init({
+        offset: 2000,
+        throttle: 200,
+        firstnum: 3
       })
-      this.descHtml = res.data
-      this.setHeight()
     }
   }
 }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 .rich-details {
   margin-top: 0.2rem;
   background-color: #fff;
@@ -113,8 +167,8 @@ export default {
     }
   }
   .product-desc {
-    .desc-iframe {
-      width: 100%;
+    p {
+      margin: 0;
     }
   }
 }
