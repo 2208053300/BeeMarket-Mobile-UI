7f99ef20e67acea9f36d11f45ba3c2508781fe43
yangchao930915 <2208053300@qq.com>
2019-06-27 16:31:53 +0800

    接入网易七鱼

--- src/pages/BeeHelper/components/ContactCustomer.vue
+++ src/pages/BeeHelper/components/ContactCustomer.vue
@@ -19,7 +19,9 @@ export default {
   data() {
     return {
       // 获取app cookies中的 token
-      test1: ''
+      test1: '',
+      // 网易七鱼
+      isSdkReady: false
     }
   },
   computed: {
@@ -32,7 +34,20 @@ export default {
 
   },
   mounted() {
+    (function(w, d, n, a, j) {
+      w[n] = w[n] || function() {
+        (w[n].a = w[n].a || []).push(arguments)
+      }
+      j = d.createElement('script')
+      j.async = true
+      j.src = 'https://qiyukf.com/script/8fd8cea16e155cf0b128c4923a84282c.js?hidden=1'
+      d.body.appendChild(j)
+    })(window, document, 'ysf')
     this.test1 = 'token: ' + Cookies.get('token') + ',' + Cookies.get()
+    const _this = this
+    ysf('onready', function() {
+      _this.isSdkReady = true
+    })
   },
   methods: {
     // 此处判断浏览器环境，做出跳转
@@ -40,12 +55,21 @@ export default {
       const osObj = getOs()
       if (osObj.isWx) {
         // this.$parent.isShowCustomer = false
+        this.openService()
       } else if (osObj.isIphone && osObj.isApp) {
         window.webkit.messageHandlers.OpenService.postMessage('')
       } else if (osObj.isAndroid && osObj.isApp) {
         window.beeMarket.OpenService()
       } else {
-        // this.$parent.isShowCustomer = false
+        this.openService()
+      }
+    },
+    // 打开网易七鱼
+    async openService() {
+      if (this.isSdkReady) {
+        ysf('open')
+      } else {
+        this.$toast.fail('sdk尚未加载成功，请稍后再试')
       }
     }
   }
