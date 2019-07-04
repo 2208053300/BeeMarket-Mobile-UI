c76480954c8e0de2a01438178dcaeac41623e3a0
巧克力冰激凌 <121017405@qq.com>
2019-07-04 15:14:52 +0800

    免费送礼文件夹

---
+++ src/route/beeFreeGift.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { getToken } from '@/utils/auth'
+import { getOs } from '@/utils'
+
+Vue.use(Router)
+
+const router = new Router({
+  // NOTE 返回后再次进入定位到最顶部
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    {
+      path: '',
+      name: 'beeFreeGift',
+      component: () => import('@/pages/BeeFreeGift'),
+      children: [
+        {
+          path: '',
+          component: () => import('@/pages/BeeFreeGift/GiftHome')
+        }
+      ]
+    }
+  ]
+})
+router.beforeEach(async(to, from, next) => {
+  // 如果是微信未授权，等待跳转
+  const token = await getToken()
+  const osObj = getOs()
+  if (osObj.isWx && token) {
+    next()
+  } else if (!osObj.isWx) {
+    next()
+  }
+})
+export default router
