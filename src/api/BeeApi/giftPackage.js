e72b053c1633074f39edbf5a1cbde9038f1361e7
Jethro <jethrocup@gmail.com>
2019-07-09 13:21:34 +0800

    添加商品end

--- src/api/BeeApi/giftPackage.js
+++ src/api/BeeApi/giftPackage.js
@@ -1,10 +1,28 @@
-// NOTE 免费送礼
+// NOTE 农礼包
 import request from '@/utils/request'
 
 // NOTE 活动首页数据
 export function getIndexData(data) {
   return request({
     url: '/package/list',
+    method: 'post',
+    data
+  })
+}
+
+// NOTE 添加商品到农礼包
+export function packageAdd(data) {
+  return request({
+    url: '/package/add',
+    method: 'post',
+    data
+  })
+}
+
+// NOTE 我的礼包
+export function packageMine(data) {
+  return request({
+    url: '/package/mine',
     method: 'post',
     data
   })
--- src/store/modules/giftPackage.js
+++ src/store/modules/giftPackage.js
@@ -1,8 +1,10 @@
+import { packageMine } from '@/api/BeeApi/giftPackage'
 const giftPackage = {
   state: {
     selectedTotalNum: 0, // 选择总数量
     selectedTotalAmount: 0, // 选择总金额
-    showTip: true // 是否显示提示
+    showTip: true, // 是否显示提示
+    productList: [] // 礼包中的商品
   },
   mutations: {
     SET_SELECTED_TOTAL_NUM(state, num) {
@@ -13,6 +15,15 @@ const giftPackage = {
     },
     SET_SHOW_TIP(state, bool) {
       state.showTip = bool
+    }
+  },
+  actions: {
+    // 获取我的礼包信息
+    async GET_GIFT_PACKAGE_INFO({ state }) {
+      const res = await packageMine()
+      state.selectedTotalNum = res.data.selected_total_num
+      state.selectedTotalAmount = res.data.selected_total_amount
+      state.productList = res.data.product_list
     }
   }
 }
