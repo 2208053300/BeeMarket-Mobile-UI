201590069936013a01cc200ac019c12eee2ea508
yangchao930915 <2208053300@qq.com>
2019-07-10 14:03:10 +0800

    Update index.vue

--- src/pages/BeeFactory/Factory/index.vue
+++ src/pages/BeeFactory/Factory/index.vue
@@ -326,14 +326,14 @@ export default {
     async loadUID() {
       const res = await getUID()
       this.uid = res.data.uid
-      this.$toast('用户uid：' + this.uid)
+      alert('用户uid：' + this.uid)
       wxapi.wxShare({
         title: '蜂集市，等你一起轻创业',
         desc: '零风险轻创业大财富的蜂集市，邀请您成为蜂集市合伙人！',
         imgUrl: 'https://img.fengjishi.com/app/images/share_logo.jpg',
         link: `https://app.fengjishi.com/beeFactory#/?uid=${this.uid}&phone=${this.userPhone}`
       })
-      this.$toast('我出现在分享之后')
+      alert('我出现在分享之后')
     },
 
     // 跳转到入驻政策
