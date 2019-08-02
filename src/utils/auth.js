83581bc2fa7ca4b1d4b83611fc7f10eb33c06071
Jethro <jethrocup@gmail.com>
2019-08-02 15:50:53 +0800

    添加调试

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -70,6 +70,7 @@ export function checkToken() {
   } else {
     // 如果是网页端，没有token弹出手机号登录界面
     import('../route/index').then(module => {
+      console.log('跳转至登录页面')
       module.default.replace('/login')
     })
   }
