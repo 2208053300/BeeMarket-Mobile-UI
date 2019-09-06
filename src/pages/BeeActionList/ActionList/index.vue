cbde25789ae1821bc6fa728a0a9df63299f04e87
巧克力冰激凌 <121017405@qq.com>
2019-09-06 13:12:22 +0800

    助力分享图片

--- src/pages/BeeActionList/ActionList/index.vue
+++ src/pages/BeeActionList/ActionList/index.vue
@@ -134,7 +134,7 @@ export default {
           title: '蜂集市-助力公益',
           desc: '就差你了，加入蜂集市，一起助力公益',
           imgUrl:
-            'https://img.fengjishi.com.cn/product/album/2019/06/03204403fnhaQkphpQ6l19R.jpeg',
+            'https://img.fengjishi.com/app/images/share_logo.jpg',
           link: `https://app.fengjishi.com/beeActionList#/?uid=${this.uid}`
         })
       } catch (error) {
