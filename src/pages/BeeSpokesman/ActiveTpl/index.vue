3e4360054defb37ab1370852ac801e9868c165d0
巧克力冰激凌 <121017405@qq.com>
2019-09-29 09:37:05 +0800

    海报更换

--- src/pages/BeeSpokesman/ActiveTpl/index.vue
+++ src/pages/BeeSpokesman/ActiveTpl/index.vue
@@ -780,7 +780,7 @@ export default {
       .qrcode-content {
         position: absolute;
         bottom: 1rem;
-        right: 0.6rem;
+        right: 0.8;
         background-color: #fff;
         width: 1.4rem;
         height: 1.4rem;
