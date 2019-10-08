8d2bf66b1a98dfd6d1b6a128b895d3ce5b134b04
jethro <jethro_nb@qq.com>
2019-10-08 16:00:29 +0800

    修改农礼包提示文案显示策略

--- src/pages/BeeGiftPackage/components/giftPackageBar.vue
+++ src/pages/BeeGiftPackage/components/giftPackageBar.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="gift-bar flex flex-column align-center">
     <div v-if="giftPackage.showTip && giftPackage.package_recharge_balance===0" class="tips">
-      <span v-if="giftPackage.selectedTotalAmount===0">
+      <span v-if="giftPackage.package_type === 1 && giftPackage.lack_package_balance === 0">
         任意搭配满<span class="money-text"> {{ maxMoney }} 元</span>，自动生成礼包！
       </span>
-      <span v-if="giftPackage.selectedTotalAmount>0 && giftPackage.selectedTotalAmount < maxMoney">
-        还差 <span class="money-text"> {{ tipMoneyText }} 元</span>，自动生成礼包！
+      <span v-if="giftPackage.package_type === 1 && giftPackage.lack_package_balance > 0">
+        还差 <span class="money-text"> {{ giftPackage.lack_package_balance }} 元</span>，自动生成礼包！
       </span>
-      <span v-if="giftPackage.selectedTotalAmount >= maxMoney">礼包已生成</span>
+      <span v-if="giftPackage.package_type === 2">礼包已生成</span>
+      <span v-if="giftPackage.package_type === 3">未知话语</span>
     </div>
     <div v-if="giftPackage.showTip && giftPackage.package_recharge_balance > 0" class="tips">
       剩余<span class="money-text"> {{ giftPackage.package_recharge_balance }} 元</span>可使用（金额永不失效）
@@ -61,9 +62,6 @@ export default {
     ...mapState(['giftPackage']),
     canSettlement() {
       return this.giftPackage.selectedTotalAmount > 0
-    },
-    tipMoneyText() {
-      return (this.maxMoney - this.giftPackage.selectedTotalAmount).toFixed(2)
     }
   },
   watch: {},
--- src/store/modules/giftPackage.js
+++ src/store/modules/giftPackage.js
@@ -5,7 +5,9 @@ const giftPackage = {
     selectedTotalAmount: 0, // 选择总金额
     showTip: true, // 是否显示提示
     productList: [], // 礼包中的商品
-    package_recharge_balance: 0 // 礼包余额
+    package_recharge_balance: 0, // 礼包余额
+    lack_package_balance: 0, // 礼包缺少金额
+    package_type: 0 // 礼包状态  1未生成礼包 2礼包刚好生成 3礼包已生成
   },
   mutations: {
     SET_SELECTED_TOTAL_NUM(state, num) {
@@ -26,6 +28,8 @@ const giftPackage = {
       state.selectedTotalAmount = res.data.selected_total_amount
       state.productList = res.data.product_list
       state.package_recharge_balance = res.data.pgpackage_recharge_balance
+      state.lack_package_balance = res.data.lack_package_balance
+      state.package_type = res.data.package_type
     }
   }
 }
