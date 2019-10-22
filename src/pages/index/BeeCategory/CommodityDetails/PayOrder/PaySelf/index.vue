33ee95ca92ad438c95267aa22ebd703d46af8ed4
jethro <jethro_nb@qq.com>
2019-10-22 17:21:31 +0800

    修改余额支付为蜂集市支付

--- src/pages/index/BeeCategory/CommodityDetails/PayOrder/PaySelf/index.vue
+++ src/pages/index/BeeCategory/CommodityDetails/PayOrder/PaySelf/index.vue
@@ -37,11 +37,11 @@
             <div class="method-img">
               <img
                 :src="beeIcon.confirmorder_pay_icon_overage_normat"
-                alt="余额支付"
+                alt="蜂集市支付"
               >
             </div>
             <div class="method-name">
-              余额支付<span class="balance">￥{{ order.payInfo.balance }}</span>
+              蜂集市支付
             </div>
           </div>
           <div class="check-method">
@@ -95,13 +95,13 @@
             <div class="method-img">
               <img
                 :src="beeIcon.confirmorder_pay_icon_overage_disabled"
-                alt="余额支付"
+                alt="蜂集市支付"
               >
             </div>
             <div class="method-name">
               <div class="overage">
                 <span class="grey-text">
-                  余额支付
+                  蜂集市支付
                 </span>
                 <span class="grey-text2">
                   余额不足
