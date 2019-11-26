cf29bff4314d6a98bb2f23662527728de8803fa0
yangchao930915 <2208053300@qq.com>
2019-11-26 16:52:24 +0800

    接口调试

--- src/pages/BeeAlcohol/TicketList/index.vue
+++ src/pages/BeeAlcohol/TicketList/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ticket">
     <div class="subsidy text-center">
-      <p class="price">
-        10000元
+      <p :class="{price: total_amount2}">
+        {{ total_amount2 }}元
       </p>
       <p class="txt">
-        红包补贴已到账
+        {{ text }}
       </p>
     </div>
     <!-- 补贴券列表 -->
@@ -14,32 +14,23 @@
         红包提现
       </p>
       <van-row>
-        <van-col span="12">
-          <div class="ticket-item text-center">
-            <img :src="beeIcon.toCash" class="corner-img">
+        <!-- type 类型 1可提现 2可转为余额 -->
+        <!-- status 状态 1未获得 2已使用 3立即使用 4立即提现 5转为余额 -->
+        <van-col v-for="(item, index) in listData" :key="index" span="12">
+          <div class="ticket-item text-center" :class="{used: item.status===2}">
+            <img v-if="item.type===1 && [1,4].includes(item.status)" :src="beeIcon.toCash" class="corner-img">
+            <img v-if="item.type===1 && item.status===2" :src="beeIcon.notCash" class="corner-img">
+            <img v-if="item.type===2 && [1,5].includes(item.status)" :src="beeIcon.canTurn" class="corner-img">
+            <img v-if="item.type===2 && item.status===2" :src="beeIcon.notTurn" class="corner-img">
             <p class="ticket-price">
-              2000<span>元</span>
+              {{ item.amount }}<span>元</span>
             </p>
             <div class="flex flex-center">
-              <button class="btn flex flex-center align-center">
-                <span>立即提现</span>
-                <img :src="beeIcon.arrow" class="icon-img">
+              <button v-if="[1,2].includes(item.status)" class="btn flex flex-center align-center not-get">
+                <span>{{ item.status_text }}</span>
               </button>
-            </div>
-            <p class="tip">
-              邀请直接蜂友购买<span>1箱</span>立即激活
-            </p>
-          </div>
-        </van-col>
-        <van-col span="12">
-          <div class="ticket-item text-center used">
-            <img :src="beeIcon.toCash" class="corner-img">
-            <p class="ticket-price">
-              2000<span>元</span>
-            </p>
-            <div class="flex flex-center">
-              <button class="btn flex flex-center align-center">
-                <span>立即提现</span>
+              <button v-else class="btn flex flex-center align-center" @click="btnAction(item.id,item.status)">
+                <span>{{ item.status_text }}</span>
                 <img :src="beeIcon.arrow" class="icon-img">
               </button>
             </div>
@@ -60,9 +51,13 @@
 
 <script>
 // import Rule from './components/rule'
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 import { getUID } from '@/api/BeeApi/user'
-import { showShareIcon, setShareOptions } from '@/utils/share'
+import { getTicketListData, toBalance } from '@/api/BeeApi/alcohol'
+import {
+  // showShareIcon,
+  setShareOptions
+} from '@/utils/share'
 
 export default {
   metaInfo: {
@@ -72,7 +67,7 @@ export default {
   props: {},
   data() {
     return {
-      showControls: false,
+
       beeIcon: {
         arrow: require('@/assets/icon/alcohol/liqueur_red_envelope_icon_triangle.png'),
         toCash: require('@/assets/icon/alcohol/liqueur_red_envelope_label_mention_n.png'), // 可提现
@@ -82,14 +77,19 @@ export default {
         buyBtn: require('@/assets/icon/alcohol/liqueur_red_envelope_button_buy.png'), // 不可转余额
         shareBtn: require('@/assets/icon/alcohol/liqueur_red_envelope_button_share.png') // 不可转余额
       },
-      showRule: false,
-      uid: 0
+
+      uid: 0,
+      // listData
+      listData: [],
+      text: '',
+      total_amount2: 0
     }
   },
   computed: {},
   watch: {},
   created() {},
   mounted() {
+    this.getListData()
     // this.initShare()
     // 如果有store说明该页面作为组件在webApp显示
     if (this.$store) {
@@ -102,7 +102,32 @@ export default {
     // showShareIcon()
   },
   methods: {
+    // 获取现金券列表数据
+    async getListData() {
+      const res = await getTicketListData()
 
+      this.text = res.data.text
+      this.total_amount2 = res.data.total_amount
+      console.log(this.total_amount2)
+      this.listData = res.data.lists
+    },
+    // 列表按钮操作 id:现金券id status: 3 立即使用 4 立即提现 5 转为余额
+    async btnAction(id, status) {
+      let res
+      if (status === 3) {
+        this.$router.push('/')
+      } else if (status === 4) {
+        window.location.href = window.location.origin + '/beeFriends#/pay'
+      } else if (status === 5) {
+        res = await toBalance({ id })
+        this.$toast(res.message)
+        setTimeout(() => {
+          this.getTicketListData()
+        }, 2000)
+      } else {
+        console.log('其他，出问题了额，status:', status)
+      }
+    },
     // 分享
     async initShare() {
       try {
@@ -127,6 +152,7 @@ export default {
   height: 100%;
   position: fixed;
   top: 0;
+  overflow-y: auto;
   box-sizing: border-box;
    background:linear-gradient(to top, rgba(232,99,58,1),rgba(213,24,14,1));
   .subsidy{
@@ -142,12 +168,16 @@ export default {
     font-size: 1.2rem;
     margin:0 auto 0.1rem;
     font-weight: 600;
+    color: #E57F35;
     background-image:-webkit-linear-gradient(bottom,red,#fd8403,yellow);
     -webkit-background-clip:text;
-    -webkit-text-fill-color:transparent; }
+    -webkit-text-fill-color:transparent;
+    }
   .txt{font-size: .28rem;color:#ef5821; margin: 0;font-weight: 600;}
 
+  .van-col{ margin-bottom: 0.2rem;}
   .ticket-box{
+    margin-bottom: 1.2rem;
     .title{
       color:#CD9761;
       font-size: .36rem;
@@ -168,6 +198,7 @@ export default {
       position: relative;
       box-sizing: border-box;
       padding-top: 0.5rem;
+
       .corner-img{
         position: absolute;
         top: 0;
@@ -186,6 +217,9 @@ export default {
         .icon-img{
           width: .14rem;height: .16rem;margin-left: .1rem;
         }
+      }
+      .btn.not-get{
+        background-color: #999;
       }
       .tip{
         font-size:.22rem ;color:#999;
