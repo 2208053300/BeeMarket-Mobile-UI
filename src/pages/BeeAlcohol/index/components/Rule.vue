118676c5162b893a33ceee2735af0feec5ceaed7
jethro <jethro_nb@qq.com>
2019-12-04 20:22:11 +0800

    调整字间距3

--- src/pages/BeeAlcohol/index/components/Rule.vue
+++ src/pages/BeeAlcohol/index/components/Rule.vue
@@ -78,7 +78,7 @@ export default {
     padding-left: 0.28rem;
     padding-right: 0.37rem;
     color: white;
-    line-height: 1.2;
+    line-height: 1.6;
     font-size: 0.3rem;
     .dl {
       margin-bottom: 0.3rem;
