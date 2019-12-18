81843c5d2cd28073beb2d050608668f14cd27f05
yangchao930915 <2208053300@qq.com>
2019-12-18 17:34:23 +0800

    Update index.vue

--- src/pages/BeeFriends/Pay/index.vue
+++ src/pages/BeeFriends/Pay/index.vue
@@ -209,9 +209,9 @@ export default {
     return {
       title: '提现',
       // 姓名
-      name: '杨超',
+      name: '',
       nameError: false,
-      idNo: '50023819920805397x',
+      idNo: '',
       idNoError: false,
       canNext: false,
       // 金额
